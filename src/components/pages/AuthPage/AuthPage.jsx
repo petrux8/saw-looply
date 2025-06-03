2c398f28b8375dab3d7e94236279d6d9a7132b60
petrux8 <matildepetrucci2003@gmail.com>
2025-06-03 14:03:07 +0200

    migliorato auth responsive

--- src/components/pages/AuthPage/AuthPage.jsx
+++ src/components/pages/AuthPage/AuthPage.jsx
@@ -9,7 +9,7 @@ import Alert from "../../Alert";
 import { useState } from "react";
 import { GoogleLoginButton } from "./GoogleLoginButton";
 
-const AuthPage = () => {
+export default function AuthPage() {
   const [authType, setAuthType] = useState("login");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -74,85 +74,78 @@ const AuthPage = () => {
 
   return (
     <div className="auth-container">
-      <Alert message={error} type="error" onClose={() => setError("")} />
-      <Alert message={success} type="success" onClose={() => setSuccess("")} />
-      <div className="box-container">
-        <div className="row border rounded-5 p-3 bg-white shadow box-area">
-          <div className="col-md-6 rounded-4 auth-left">
-            <img src="/logo.png" className="app-logo" alt="logo" />
-          </div>
+      <div className="row d-flex rounded-5 shadow-lg box-container">
+        {/* Sezione Sinistra - Immagine */}
+        <div className="col-md-6 p-4 auth-left">
+          <img src="/logo.png" className="app-logo" alt="logo" />
+        </div>
 
-          <div className="col-md-6 auth-right">
-            <div className="row align-items-center">
-              <h2>{authType === "login" ? "Hello, Again" : "Welcome"}</h2>
-              <p>
-                {authType === "login"
-                  ? "We are happy to have you back."
-                  : "Create your account to get started."}
-              </p>
-              <form
-                onSubmit={authType === "login" ? handleLogin : handleRegister}
-              >
-                <div className="mb-3">
-                  <label htmlFor="email" className="form-label">
-                    Email address
-                  </label>
-                  <input
-                    type="email"
-                    className="form-control"
-                    id="email"
-                    placeholder="Enter your email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                  />
-                </div>
-                <div className="mb-3">
-                  <label htmlFor="password" className="form-label">
-                    Password
-                  </label>
-                  <input
-                    type="password"
-                    className="form-control"
-                    id="password"
-                    placeholder="Enter your password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                  />
-                </div>
-                {authType === "login" && (
-                  <div className="d-flex justify-content-between align-items-center mb-3">
-                    <a href="#" className="text-primary">
-                      Forgot Password?
-                    </a>
-                  </div>
-                )}
-                <button type="submit" className="btn btn-primary w-100 mb-3">
-                  {authType === "login" ? "Login" : "Sign Up"}
-                </button>
-                <GoogleLoginButton
-                  authType={authType}
-                  onClick={handleGoogleSignIn}
-                />
-              </form>
-              <div className="mt-3">
-                <p>
-                  {authType === "login"
-                    ? "Don't have an account? "
-                    : "Already have an account? "}
-                  <button
-                    className="btn btn-link p-0 align-baseline"
-                    onClick={handleAuthSwitch}
-                  >
-                    {authType === "login" ? "Sign Up" : "Login"}
-                  </button>
-                </p>
-              </div>
+        {/* Sezione Destra - Form di Login */}
+        <div className="col-md-6 p-4 bg-white auth-right">
+          <h2>{authType === "login" ? "Hello, Again" : "Welcome"}</h2>
+          <p>
+            {authType === "login"
+              ? "We are happy to have you back."
+              : "Create your account to get started."}
+          </p>
+
+          <form onSubmit={authType === "login" ? handleLogin : handleRegister}>
+            <div className="mb-3">
+              <label htmlFor="email" className="form-label">
+                Email
+              </label>
+              <input
+                type="email"
+                className="form-control"
+                id="email"
+                placeholder="Enter your email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
             </div>
+            <div className="mb-3">
+              <label htmlFor="password" className="form-label">
+                Password
+              </label>
+              <input
+                type="password"
+                className="form-control"
+                id="password"
+                placeholder="Enter your password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            {authType === "login" && (
+              <div className="d-flex justify-content-between align-items-center mb-3">
+                <a href="#" className="text-primary">
+                  Forgot Password?
+                </a>
+              </div>
+            )}
+            <button type="submit" className="btn btn-primary w-100 mb-3">
+              {authType === "login" ? "Login" : "Sign Up"}
+            </button>
+            <GoogleLoginButton
+              authType={authType}
+              onClick={handleGoogleSignIn}
+            />
+          </form>
+          <div className="mt-3">
+            <p>
+              {authType === "login"
+                ? "Don't have an account? "
+                : "Already have an account? "}
+              <button
+                className="btn btn-link p-0 align-baseline"
+                onClick={handleAuthSwitch}
+              >
+                {authType === "login" ? "Sign Up" : "Login"}
+              </button>
+            </p>
           </div>
         </div>
       </div>
     </div>
   );
-};
-
-export default AuthPage;
+}
